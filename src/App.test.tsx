5148b2db4d0d054b09b71d78efff2ef40bbd65b4
Romain Breton <rbreton@MacBook-Pro-de-Romain.local>
2018-12-16 09:31:40 +0100

    Update dependencies of the banch

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
-// import App from './App';
+// import { Header } from 'Components';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
